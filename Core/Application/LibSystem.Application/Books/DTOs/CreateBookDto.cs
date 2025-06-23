003c2cd9bc92e392f43ab1d8b623c71ed108b22a
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-23 18:21:13 +0530

    implemented DTO classes

---
+++ Core/Application/LibSystem.Application/Books/DTOs/CreateBookDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSystem.Application.Books.DTOs
+{
+    public class CreateBookDto
+    {
+        [Required]
+        [StringLength(200, MinimumLength = 1)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        public string Author { get; set; } = string.Empty;
+
+        [Range(1450, 2024)]
+        public int PublicationYear { get; set; }
+
+        [Required]
+        [Range(0, 2)]
+        public int Category { get; set; }
+    }
+}
---
+++ Core/Application/LibSystem.Application/Books/DTOs/CreateMemberDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSystem.Application.Books.DTOs
+{
+    public class CreateMemberDto
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0, 2)]
+        public int MemberType { get; set; }
+    }
+}
