67fa8e8a9a2d75e3d9cdb4b243becce7b5ac226a
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-23 11:05:53 +0530

    implemented events for book borrowing retur creating and member creating

---
+++ Core/Domain/LibSystem.Domain/Events/BookCreatedEvent.cs
@@ -0,0 +1,28 @@
+﻿using LibSystem.Domain.Common;
+using LibSystem.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSystem.Domain.Events
+{
+    public sealed class BookCreatedEvent : IDomainEvent
+    {
+        public BookId BookId { get; }
+        public string Title { get; }
+        public string Author { get; }
+        public string Category { get; }
+        public DateTime OccurredOn { get; }
+
+        public BookCreatedEvent(BookId bookId, string title, string author, string category)
+        {
+            BookId = bookId;
+            Title = title;
+            Author = author;
+            Category = category;
+            OccurredOn = DateTime.UtcNow;
+        }
+    }
+}
---
+++ Core/Domain/LibSystem.Domain/Events/MemberCreatedEvent.cs
@@ -0,0 +1,26 @@
+﻿using LibSystem.Domain.Common;
+using LibSystem.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSystem.Domain.Events
+{
+    public sealed class MemberCreatedEvent : IDomainEvent
+    {
+        public MemberId MemberId { get; }
+        public Name MemberName { get; }
+        public string MemberType { get; }
+        public DateTime OccurredOn { get; }
+
+        public MemberCreatedEvent(MemberId memberId, Name memberName, string memberType)
+        {
+            MemberId = memberId;
+            MemberName = memberName;
+            MemberType = memberType;
+            OccurredOn = DateTime.UtcNow;
+        }
+    }
+}
