ffab23685df97ae31f6de3ea9f3932fc174968f2
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-23 09:37:07 +0530

    completed the common classes of the domain layer

---
+++ Core/Domain/LibSystem.Domain/Common/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSystem.Domain.Common
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+        bool HasActiveTransaction { get;
+        }
+}
