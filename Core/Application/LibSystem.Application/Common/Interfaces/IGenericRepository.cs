afdb9bac72308c8bf6edf34d7ec2bf7e62d2eade
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-24 12:37:15 +0530

    created generic class for repository handling in the application layer and started on command handlers

---
+++ Core/Application/LibSystem.Application/Common/Interfaces/IGenericRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSystem.Application.Common.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        // Query operations
+        Task<T> GetByIdAync(int id, CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<T>> GetAllAync(CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<T>> FindAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
+        Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
+        Task<int> CountAsync(CancellationToken cancellationToken = default);
+        Task<int> CountAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
+
+        // Command operations
+        Task AddAsync(T entity, CancellationToken cancellationToken = default);
+        Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default);
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+        // Unit of work operations
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+
+}
