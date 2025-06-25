349dcc2d583659d871109a7c1dc7bfa6af365341
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-25 12:51:20 +0530

    refactoring project reference

---
+++ Infrastructure/Persistence/LibSystem.Persistence/LibraryDbContext.cs
@@ -0,0 +1,30 @@
+﻿using LibSystem.Application.Common.Interfaces;
+using LibSystem.Domain.Entities.Books;
+using LibSystem.Domain.Entities.Borrowing;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSystem.Persistence
+{
+    public class LibraryDbContext : DbContext, IApplicationDbContext, IUnitOfWork
+    {
+        private readonly IMediator _mediator;
+        private IDbContextTransaction _currentTransaction;
+
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options, IMediator mediator)
+            : base(options)
+        {
+            _mediator = mediator;
+        }
+
+        // DbSets for all domain entities
+        public DbSet<Book> Books => Set<Book>();
+        public DbSet<Member> Members => Set<Member>();
+        public DbSet<BorrowingRecord> BorrowingRecords => Set<BorrowingRecord>();
+    }
