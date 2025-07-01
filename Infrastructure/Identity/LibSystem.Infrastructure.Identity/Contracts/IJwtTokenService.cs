bf75e9c1fb17c5b8b509eb575cf253988e0dcaad
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-07-01 09:04:54 +0530

    added potential contracts might update later

---
+++ Infrastructure/Identity/LibSystem.Infrastructure.Identity/Contracts/IJwtTokenService.cs
@@ -0,0 +1,18 @@
+﻿using LibSystem.Infrastructure.Identity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSystem.Infrastructure.Identity.Contracts
+{
+    public interface IJwtTokenService
+    {
+        Task<string> GenerateTokenAsync(ApplicationUser user, IList<string> roles);
+        Task<string> RefreshTokenAsync();
+        ClaimsPrincipal? GetPrincipalFromExpiredToken(string token);
+        Task<bool> ValidateTokenAsync(string token);
+    }
+}
---
+++ Infrastructure/Identity/LibSystem.Infrastructure.Identity/Contracts/IUserManagementService.cs
@@ -0,0 +1,23 @@
+﻿using LibSystem.Application.Common.Models;
+using LibSystem.Infrastructure.Identity.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSystem.Infrastructure.Identity.Contracts
+{
+    public interface IUserManagementService
+    {
+        Task<Result<IReadOnlyList<UserDto>>> GetAllUsersAsync();
+        Task<Result<UserDto>> GetUserByIdAsync(int userId);
+        Task<Result<UserDto>> GetUserByEmailAsync(string email);
+        Task<Result<UserDto>> UpdateUserAsync(int userId, UpdateUserRequest request);
+        Task<Result> DeactivateUserAsync(int userId);
+        Task<Result> ActivateUserAsync(int userId);
+        Task<Result> AssignRoleAsync(AssignRoleRequest request);
+        Task<Result> RemoveRoleAsync(int userId, string role);
+        Task<Result<IReadOnlyList<string>>> GetUserRolesAsync(int userId);
+    }
+}
